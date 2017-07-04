a36f7e2b3a6cac5f5b52db04ba5b7bfe12ae8c2e
marialbst <marialbst@gmail.com>
2017-07-05 00:11:36 +0300

    inheritance lab added

---
+++ InheritanceLab/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+public class RandomList : ArrayList
+{
+    private Random rnd;
+
+    public RandomList()
+    {
+        this.rnd = new Random();
+    }
+
+    public string RandomString()
+    {
+        int index = rnd.Next(0, this.Count - 1);
+        return (string)this[index];
+    }
+}
---
+++ InheritanceLab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public void Push(string item)
+    {
+        this.data.Add(item);
+    }
+
+    public string Pop()
+    {
+        var elem = this.data.Last();
+        this.data.Remove(elem);
+        return elem;
+    }
+
+    public string Peek()
+    {
+        return this.data.Last();
+    }
+
+    public bool IsEmpty()
+    {
+        return this.data.Count > 0;
+    }
+}
