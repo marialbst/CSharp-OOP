c4cc23ee5a244f2952c40c8f83d03d271a309e70
marialbst <marialbst@gmail.com>
2017-07-05 16:57:02 +0300

    added task 1 and 2

--- InheritanceExercise/01.Person/Program.cs
+++ InheritanceExercise/01.Person/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.Person
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
         }
     }
 }
