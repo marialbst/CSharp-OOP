0dd138e18afd9037de0bd93ddefc8da846d10e70
marialbst <marialbst@gmail.com>
2017-07-13 21:16:06 +0300

    refactoring

---
+++ Avatar/Avatar/Core/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Engine
+{
+    private NationsBuilder builder;
+
+    public Engine()
+    {
+        this.builder = new NationsBuilder();
+    }
+
+    public void Run()
+    {
+        string command = Console.ReadLine();
+
+        while (command != "Quit")
+        {
+            List<string> inputArgs = ParseCommand(command);
+
+            ProcessCommand(inputArgs);
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine(builder.GetWarsRecord());
+    }
+
+    private void ProcessCommand(List<string> inputArgs)
+    {
+        var command = inputArgs[0];
+
+        switch (command)
+        {
+            case "Bender":
+                builder.AssignBender(inputArgs);
+                break;
+            case "Monument":
+                builder.AssignMonument(inputArgs);
+                break;
+            case "War":
+                builder.IssueWar(inputArgs[1]);
+                break;
+            case "Status":
+                Console.WriteLine(builder.GetStatus(inputArgs[1]));
+                break;
+        }
+    }
+
+    private List<string> ParseCommand(string command)
+    {
+        return command.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+}
--- Avatar/Avatar/Startup.cs
+++ Avatar/Avatar/Startup.cs
@@ -5,32 +5,7 @@ class Startup
 {
     static void Main(string[] args)
     {
-        NationsBuilder builder = new NationsBuilder();
-        string command = Console.ReadLine();
-
-        while (command != "Quit")
-        {
-            var inputArgs = command.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
-
-            switch (inputArgs[0])
-            {
-                case "Bender":
-                    builder.AssignBender(inputArgs);
-                    break;
-                case "Monument":
-                    builder.AssignMonument(inputArgs);
-                    break;
-                case "War":
-                    builder.IssueWar(inputArgs[1]);
-                    break;
-                case "Status":
-                    Console.WriteLine(builder.GetStatus(inputArgs[1]));
-                    break;
-            }
-             
-            command = Console.ReadLine();
-        }
-
-        Console.WriteLine(builder.GetWarsRecord());
+        Engine engine = new Engine();
+        engine.Run();
     }
 }
